552504db8e52509bdb821982d291e3366749eb12
KentseMatshira <kmatshira@yahoo.com>
2024-11-17 16:20:45 +0200

    adding preference for Drax, Rocket & Groot.

--- index.js
+++ index.js
@@ -36,6 +36,9 @@ const songs = [
 const guardians = {
     "Star-Lord": "Rock",
     "Gamora": "Pop",
+    "Drax": "Disco",
+    "Rocket": "R&B",
+    "Groot": "House"
     // Add preferences for Drax, Rocket, and Groot
 };
 
